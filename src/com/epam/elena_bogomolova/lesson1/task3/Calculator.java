a325350e4428c59b3bdfbcde668480ec326194fa
elena_bogomolova <mavakosja@gmail.com>
2017-02-09 15:05:34 +0300

    calculator created

--- src/com/epam/elena_bogomolova/lesson1/task3/Calculator.java
+++ src/com/epam/elena_bogomolova/lesson1/task3/Calculator.java
@@ -1,58 +1,106 @@
 package com.epam.elena_bogomolova.lesson1.task3;
 
-import jdk.nashorn.internal.ir.Symbol;
-
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+    private float result;
+    private float number;
+    private boolean correctinput;
+
+    public static void main(String[] args) throws IOException {
         Calculator app = new Calculator();
         app.startCalculator();
     }
 
-    private void startCalculator() {
+    private void startCalculator() throws IOException {
         printWelcomeMenu();
-        long num1 = getFirstNumber();
-        String operation = getOperation();
-        long num2 = getSecondNumber();
-        calculateAndShowResult(num1, num2, operation);
+        boolean continueworking = true;
+        while (continueworking) {
+            float num1 = getNumber(1);
+            String operation = getOperation();
+            float num2 = getNumber(2);
+            calculateAndShowResult(num1, num2, operation);
+            continueworking = checkForContinue();
+        }
+    }
 
+    private boolean checkForContinue() throws IOException {
+        System.out.println("Do you want to continue? Print N or \"no\" if you want to stop calculations. Enter any other symbol to continue.");
+        Scanner sc = new Scanner(System.in);
+        String wishtocontinue = sc.nextLine();
+        if (wishtocontinue.equalsIgnoreCase("n") || wishtocontinue.equalsIgnoreCase("no")) {
+            System.out.println("Goodbye!");
+            return false;
+        } else {
+            return true;
+        }
     }
 
     private void printWelcomeMenu() {
-        System.out.println("calculator started");
-        System.out.println("input first number, press Enter, " +
+        System.out.println("Hi, I'm Calculator! ");
+        System.out.println("I can sum (+), subtract (-), multiply (*) and divide (/) numbers for you!");
+        System.out.println();
+        System.out.println("How to work: input first number, press Enter, " +
                 "input operation sign (+, -, * or /), " +
                 "press Enter, input second number, press Enter");
+        System.out.println();
+        System.out.println("Let's begin!");
     }
 
-    private long getFirstNumber() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("hint: enter firts number and press Enter");
-        long firstNumber = Long.parseLong(sc.next());
-        sc.close();
-        return firstNumber;
+    private float getNumber(int i) {
+        correctinput = false;
+        String num = "";
+        if (i == 1) {
+            num = "first";
+        }
+        if (i == 2) {
+            num = "second";
+        }
+        System.out.println("hint: enter " + num + " number and press Enter");
+        while (!correctinput) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                number = sc.nextFloat();
+                correctinput = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect data entered! Try again. Please enter " + num + " number");
+            }
+        }
+        return number;
     }
 
     private String getOperation() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("hint: enter operation and press Enter");
-        String operation = sc.next();
-        sc.close();
+        correctinput = false;
+        String operation = null;
+        while (!correctinput) {
+            System.out.println("hint: enter operation (+, -, * or /) and press Enter");
+            Scanner sc = new Scanner(System.in);
+            operation = sc.nextLine();
+            if (operation.equals("+") || operation.equals("-") || operation.equals("*") || operation.equals("/")) {
+                correctinput = true;
+            } else System.out.println(" You entered incorrect operator, please try again");
+        }
         return operation;
     }
 
-    private long getSecondNumber() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("hint: enter second number and press Enter");
-//        long secondNumber = Long.parseLong(sc.next());
-        return Long.parseLong(sc.next());
+    private void calculateAndShowResult(float num1, float num2, String operation) {
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+        }
+        System.out.println(num1 + operation + num2 + " = " + result);
     }
-
-    private void calculateAndShowResult(long num1, long num2, String operation) {
-        System.out.println(num1 + operation + num2 + " = ");
-    }
-
-
 }
