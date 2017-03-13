e8aa370052f5919374e59a37fd4f41ea2567d0ab
ElenaVesna <mavakosja@gmail.com>
2017-03-14 00:06:08 +0300

    strings processing updated

---
+++ src/com/epam/elena_bogomolova/lesson2/Equipement.java
@@ -0,0 +1,15 @@
+package com.epam.elena_bogomolova.lesson2;
+
+
+public class Equipement {
+    private long power;
+    private boolean isTurnedOn;
+    private String place;
+
+    public Equipement(long power, boolean isTurnedOn, String place) {
+        this.power = power;
+        this.isTurnedOn = isTurnedOn;
+        this.place = place;
+    }
+
+}
