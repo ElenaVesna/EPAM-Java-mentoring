add2692e080de53e6fa5e948f477de38f752cc22
elena_bogomolova <mavakosja@gmail.com>
2017-04-05 20:11:39 +0300

    lesson 5 - initial

---
+++ src/com/epam/elena_bogomolova/lesson5/task2/DiskAnalyzer.java
@@ -0,0 +1,11 @@
+package com.epam.elena_bogomolova.lesson5.task2;
+
+
+public class DiskAnalyzer {
+    public static void main(String[] args) {
+    }
+
+    private void myMethod() {
+        System.out.println("Hi");
+    }
+}
